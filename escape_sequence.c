87fd2009eb22308ec0974ef228c78dafd31ac595
santhosh <santhoshp4136@gmail.com>
2023-06-29 11:51:51 +0530

    escape_sequence

---
+++ escape_sequence.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+ 	printf("Buzzer sound\a\n");
+ 	printf("Hi\fHello\nWorld\n");
+ 	printf("without tab space\nwith\t tap space\n");
+ 	printf("Hello\vWorld\n");
+ 	printf("First\nFirst\rLast\n");
+ 	printf("C:\\file \n");
+ 	printf("\'a\"\n\"abc\"\n");
+ 	printf("how are u\?\n");
+ 	printf("abc\b");
+ return 0;
+}
